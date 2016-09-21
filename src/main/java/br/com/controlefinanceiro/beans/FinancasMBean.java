3cfbc0b08f2adcb0750f5b1978094ff0db3745da
bolivarMardegan <bolivarMardegan@localhost>
2016-09-20 23:41:43 -0300

    Nova vers?o com TabView Todas tab est?o na mesma tabView

--- src/main/java/br/com/controlefinanceiro/beans/FinancasMBean.java
+++ src/main/java/br/com/controlefinanceiro/beans/FinancasMBean.java
@@ -1,5 +1,7 @@
 package br.com.controlefinanceiro.beans;
 
+import java.io.File;
+import java.io.IOException;
 import java.io.Serializable;
 import java.math.BigDecimal;
 import java.text.ParseException;
@@ -13,12 +15,26 @@ import java.util.List;
 import javax.annotation.PostConstruct;
 import javax.enterprise.context.SessionScoped;
 import javax.faces.application.FacesMessage;
+import javax.faces.context.ExternalContext;
+import javax.faces.context.FacesContext;
 import javax.inject.Inject;
 import javax.inject.Named;
 
+import org.apache.poi.hssf.usermodel.HSSFCell;
+import org.apache.poi.hssf.usermodel.HSSFCellStyle;
+import org.apache.poi.hssf.usermodel.HSSFRow;
+import org.apache.poi.hssf.usermodel.HSSFSheet;
+import org.apache.poi.hssf.usermodel.HSSFWorkbook;
+import org.apache.poi.hssf.util.HSSFColor;
 import org.primefaces.event.FlowEvent;
 import org.primefaces.event.RowEditEvent;
 import org.primefaces.model.TreeNode;
+
+import com.lowagie.text.BadElementException;
+import com.lowagie.text.Document;
+import com.lowagie.text.DocumentException;
+import com.lowagie.text.Image;
+import com.lowagie.text.PageSize;
 
 import br.com.controlefinanceiro.Helper.CriadroDeArvoresHelper;
 import br.com.controlefinanceiro.Helper.UsuarioSessionControler;
@@ -252,6 +268,35 @@ public class FinancasMBean extends AbstractManagedBean<Financa> implements Seria
 		return true;
 
 	}
+	
+	 public void postProcessXLS(Object document) {
+	        HSSFWorkbook wb = (HSSFWorkbook) document;
+	        HSSFSheet sheet = wb.getSheetAt(0);
+	        HSSFRow header = sheet.getRow(0);
+	         
+	        HSSFCellStyle cellStyle = wb.createCellStyle();  
+	        cellStyle.setFillForegroundColor(HSSFColor.GREEN.index);
+	        cellStyle.setFillPattern(HSSFCellStyle.SOLID_FOREGROUND);
+	         
+	        for(int i=0; i < header.getPhysicalNumberOfCells();i++) {
+	            HSSFCell cell = header.getCell(i);
+	             
+	            cell.setCellStyle(cellStyle);
+	        }
+	    }
+	     
+	    public void preProcessPDF(Object document) throws IOException, BadElementException, DocumentException {
+	        Document pdf = (Document) document;
+	        pdf.open();
+	        pdf.setPageSize(PageSize.A4);
+	 
+	        ExternalContext externalContext = FacesContext.getCurrentInstance().getExternalContext();
+	        String logo = externalContext.getRealPath("") + File.separator + "resources" + File.separator + "demo" + File.separator + "images" + File.separator + "prime_logo.png";
+	         
+	        pdf.add(Image.getInstance(logo));
+	    }
+	
+	
 
 	public Financa getFinanca() {
 		return financa;
